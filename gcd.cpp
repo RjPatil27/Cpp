43ec1aa1ce1b80fe115294514bb20ba2bacaca27
Rajat Patil <30958676+RjPatil27@users.noreply.github.com>
2018-03-03 15:45:28 +0000

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,26 @@
+//Example to find the GCD of two positive integers (entered by the user) using recursion in C programming.
+
+#include <iostream>
+using namespace std;
+
+int hcf(int n1, int n2);
+
+int main()
+{
+   int n1, n2;
+
+   cout << "Enter two positive integers: ";
+   cin >> n1 >> n2;
+
+   cout << "H.C.F of " << n1 << " & " <<  n2 << " is: " << hcf(n1, n2);
+
+   return 0;
+}
+
+int hcf(int n1, int n2)
+{
+    if (n2 != 0)
+       return hcf(n2, n1 % n2);
+    else 
+       return n1;
+}
